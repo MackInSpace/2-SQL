8d192a86b64a0743888b0e93111d16b13c824ea0
MackInSpace <amackey@lssu.edu>
2025-08-03 15:52:43 -0600

    sql inputs

--- week1/sql_examples.py
+++ week1/sql_examples.py
@@ -15,3 +15,30 @@ CREATE TABLE drivers (
 
 ALTER TABLE drivers ADD CONSTRAINT fk_drivers_cars FOREIGN KEY (car_id) REFERENCES cars;
 """
+
+"""CREATE TABLE events (
+	id SERIAL,
+	show_time TIMESTAMP, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE auditoria (
+	id SERIAL,
+	capacity INTEGER NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE films (
+	id SERIAL,
+	title TEXT NOT NULL,
+	runtime INTEGER,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE events (
+	id SERIAL,
+	show_time TIMESTAMP,
+	PRIMARY KEY (id),
+	auditorium_id INT,
+	film_id INT NOT NULL
+);"""
