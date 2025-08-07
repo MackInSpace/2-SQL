2e636b1e54ee109354485a2176161f1a4bbd4c4f
MackInSpace <amackey@lssu.edu>
2025-08-06 19:13:06 -0600

    cc aggregate queries

--- week2/aggregations.sql
+++ week2/aggregations.sql
@@ -8,3 +8,16 @@ GROUP BY genre;
 SELECT genre, COUNT(*) FROM books
 GROUP BY genre
 HAVING COUNT(*) > 1;"""
+
+"""SELECT customer_id, MIN(order_date) FROM orders 
+GROUP BY customer_id 
+ORDER BY customer_id;
+
+SELECT customer_id, AVG(freight) AS avg_freight FROM orders 
+GROUP BY customer_id 
+ORDER BY avg_freight;
+
+SELECT o.order_id, count(DISTINCT product_id) AS product_count 
+FROM order_details o 
+GROUP BY order_id HAVING(count(*)) >= 5 
+ORDER BY product_count DESC;"""
