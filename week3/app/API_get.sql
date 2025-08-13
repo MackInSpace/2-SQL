4b6e5f65aa33d2b635d155c1c14dad28382f4359
MackInSpace <amackey@lssu.edu>
2025-08-13 17:26:51 -0600

    get request done

--- week3/app/API_get.sql
+++ week3/app/API_get.sql
@@ -1,1 +1,13 @@
 -- These are all in the Insomnia environment
+
+"""{{baseURL}}/zen
+
+{{baseURL}}/users/torvalds
+
+{{baseURL}}/orgs/kong
+
+{{baseURL}}/orgs/kong/repos
+
+{{baseURL}}/repos/kong/insomnia
+
+{{baseURL}}/repos/kong/insomnia/pulls"""
