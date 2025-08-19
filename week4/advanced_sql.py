2510dc2d6d2fd3873f765f44c7ff50eb9ea797b0
MackInSpace <amackey@lssu.edu>
2025-08-19 13:14:01 -0600

    json in sql

--- week4/advanced_sql.py
+++ week4/advanced_sql.py
@@ -36,3 +36,29 @@ frame_height,
 frame_width * frame_height AS frame_area
 FROM frames
 GROUP BY frame_width, frame_height, frame_area;"""
+
+"""SELECT * FROM moma_artists LIMIT 50;"""
+
+"""SELECT jsonb_pretty(info) AS formatted_info
+FROM moma_artists LIMIT 50;"""
+
+"""SELECT 
+info -> 'display_name' AS name,
+info -> 'nationality' as nationality
+FROM moma_artists
+ORDER BY id
+LIMIT 50;"""
+
+"""SELECT 
+info -> 'display_name' AS name,
+info -> 'nationality' as nationality
+FROM moma_artists
+WHERE info ->> 'nationality' = 'American'
+ORDER BY id
+LIMIT 50;"""
+
+"""INSERT INTO moma_artists (info) VALUES (
+    json_object('{display_name, Ablade Glover, nationality, Ghanaian}')
+);"""
+
+"""SELECT info FROM moma_artists ORDER BY id DESC LIMIT 1;"""
