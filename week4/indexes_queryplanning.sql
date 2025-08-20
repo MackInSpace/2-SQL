9b942652d828e5b62f4bd3c61d55a9583e8f1783
MackInSpace <amackey@lssu.edu>
2025-08-20 13:58:50 -0600

    query planning

--- week4/indexes_queryplanning.sql
+++ week4/indexes_queryplanning.sql
@@ -14,3 +14,14 @@ CREATE INDEX moma_works_hash_index ON moma_works USING HASH (artist);"""
 """SELECT title FROM moma_works WHERE artist = 'Frank Lloyd Wright';"""
 
 """DROP INDEX moma_works_hash_index;"""
+
+"""EXPLAIN SELECT date_acquired FROM moma_works 
+WHERE date_acquired BETWEEN '1950-01-01' AND '1959-12-31';"""
+
+"""EXPLAIN ANALYZE SELECT date_acquired FROM moma_works
+WHERE date_acquired BETWEEN '1950-01-01' AND '1959-12-31';"""
+
+"""CREATE INDEX date_acq_idx ON moma_works(date_acquired);"""
+
+"""EXPLAIN ANALYZE SELECT date_acquired FROM moma_works 
+WHERE date_acquired BETWEEN '1950-01-01' AND '1959-12-31';"""
