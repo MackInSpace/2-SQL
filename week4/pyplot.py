d64aa0f18d4040f7f4645db48f0cd9c3af558b56
MackInSpace <amackey@lssu.edu>
2025-08-21 11:32:58 -0600

    pyplot

---
+++ week4/pyplot.py
@@ -0,0 +1,32 @@
+"""This code is on colab.research.google.com"""
+
+"""import numpy as np
+import matplotlib.pyplot as plt"""
+
+"""x1 = [0.1, 0.2, 0.3, 0.4]
+y1 = [0.5, 2.2, 3.2, 8.7]
+x2 = [0.1, 0.2, 0.3, 0.4]
+y2 = [1, 4, 9, 16]
+
+plt.plot(x1, y1, label='first plot')
+plt.plot(x2, y2, label='second plot')
+plt.legend()"""
+
+"""The NumPy library
+Short for Numerical Python
+
+Includes a data structure called an ndarray (n-dimensional array, NumPy array):
+
+Similar to lists, but designed for data science
+More efficient storage & access for large amounts of numbers
+More built-in functionality for working with numbers"""
+
+"""num = 100
+x = np.linspace(0, 20, num)
+print(x)"""
+
+"""y = np.random.rand(num)
+print(y)"""
+
+"""plt.plot(x, y)"""
+
