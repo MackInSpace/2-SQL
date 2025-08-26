c3ccc451c4dafce3ae61f061c60526a06799e940
MackInSpace <amackey@lssu.edu>
2025-08-26 14:23:19 -0600

    week off testing

---
+++ week4/weekoff.py
@@ -0,0 +1,7 @@
+"""import fanstatic
+from fanstatic import Library
+
+library = Library('weekoff', 'static')
+
+weekoff_css = fanstatic.Resource(
+    library, 'weekoff.css', minified='weekoff.min.css')"""
